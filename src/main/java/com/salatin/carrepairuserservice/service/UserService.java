7e0a69c9e8019bdeda9582159b34e9ae79036d81
Serhii Salatin <lifenjoy8888@gmail.com>
2023-02-09 15:00:31 +0200

    UserService with save mtd was created

---
+++ src/main/java/com/salatin/carrepairuserservice/service/UserService.java
@@ -0,0 +1,7 @@
+package com.salatin.carrepairuserservice.service;
+
+import com.salatin.carrepairuserservice.model.User;
+
+public interface UserService {
+    User save(User user);
+}
---
+++ src/main/java/com/salatin/carrepairuserservice/service/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.salatin.carrepairuserservice.service.impl;
+
+import com.salatin.carrepairuserservice.model.User;
+import com.salatin.carrepairuserservice.repository.UserRepository;
+import com.salatin.carrepairuserservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private UserRepository userRepository;
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
