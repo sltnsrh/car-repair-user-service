268201ba66617f8192d24d04ad3b788da5d1041b
Serhii Salatin <lifenjoy8888@gmail.com>
2023-02-09 12:20:34 +0200

    config for mongo was added

--- src/main/java/com/salatin/carrepairuserservice/model/User.java
+++ src/main/java/com/salatin/carrepairuserservice/model/User.java
@@ -1,9 +1,17 @@
 package com.salatin.carrepairuserservice.model;
 
 import java.util.List;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
+@Document
 public class User {
+    @Id
     private Long id;
+    @Indexed(unique = true)
     private String email;
     private String password;
     private String firstName;
