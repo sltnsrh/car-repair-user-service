ca495034492a65c73b3e99495e8e816b660570b8
Serhii Salatin <lifenjoy8888@gmail.com>
2023-02-15 14:34:26 +0200

    project structure was rebuilt

--- src/main/java/com/salatin/carrepairuserservice/UserServiceApplication.java
+++ src/main/java/com/salatin/carrepairuserservice/UserServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarRepairUserServiceApplication {
+public class UserServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CarRepairUserServiceApplication.class, args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 
 }
